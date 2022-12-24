1ad3a7c6cf6fc66812041f799ce370609bb1ee72
Esteban <stevepetes26@gmail.com>
2022-12-24 18:49:48 +0300

    React Testing Deployment

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Steven Modimo React Test</h1>
+      <p>SMRT</p>
+    </div>
+  )
+}
+
+export default App
