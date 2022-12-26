7ac302770247db1ae4b61a6168ae8f0151debddc
Esteban <stevepetes26@gmail.com>
2022-12-26 21:02:13 +0300

    React Testing Deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createRoot}from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import Navbar from './components/navbar'
+import Home from './routes/home'
+import About from './routes/about'
+import Projects from './routes/projects'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const AppLayOut = () => {
+  return (
+    <>
+    <Navbar />
+    <Outlet />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayOut />,
+    path: '/',
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'projects',
+        element: <Projects />
+      }
+    ]
+  }
+])
+
+
+createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
 )
